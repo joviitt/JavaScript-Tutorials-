88cddb6b8dc4863d5c976c510ffe050ed145f1ba
joviitt <jovitjohn2004@gmail.com>
2025-12-24 13:18:23 +0530

    variables

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
 console.log("Jovit")
-hello
+
+// variable declaration
+
+const accountId = 112344
+let accountEmail = "jovitjohn2004@gmail.com"
+var password = "12345"
+
+accountCity = "Delhi"
+
+console.log(accountId);
