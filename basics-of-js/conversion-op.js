3c443cce50bf8af66ad5da3cbb39adbf9ba3c0ca
joviitt <jovitjohn2004@gmail.com>
2025-12-25 00:31:19 +0530

    CONVERSION

---
+++ basics-of-js/conversion-op.js
@@ -0,0 +1,10 @@
+let score = "33"
+
+console.log(typeof score); // typeof is used to find the data type of the variable or any value that is given as its argument
+console.log(typeof(score)); // can be used as a function as well
+
+let valueasNum = Number(score) // conversion to NUMBER
+
+console.log(valueasNum)
+console.log(score)
+
