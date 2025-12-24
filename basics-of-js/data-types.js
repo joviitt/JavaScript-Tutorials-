8939d2b4e8245e75416bc3664a9db562e5d8ebf2
joviitt <jovitjohn2004@gmail.com>
2025-12-24 21:38:18 +0530

    documentation info

--- basics-of-js/data-types.js
+++ basics-of-js/data-types.js
@@ -1,1 +1,10 @@
 "use strict";  // this is not required, but its purpose is that it treats/processes all the JS CODE as the newer version of JS
+
+// alert(3+3)  this is used for browser, but we are using nodejs
+
+console.log(
+    3+3) // js ignores whitespaces here but the readability is poor. READABILITY should be the first priority.
+
+// ECMAscript(tc39.es) and mdn web docs(Mozilla) are some documentations we can refer for learning JS standards
+
+let name
