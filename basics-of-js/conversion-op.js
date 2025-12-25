c2795b39ca18211827ea8c889856e819b79c3b9c
joviitt <jovitjohn2004@gmail.com>
2025-12-26 00:19:53 +0530

    conversion-errors

--- basics-of-js/conversion-op.js
+++ basics-of-js/conversion-op.js
@@ -1,10 +1,15 @@
-let score = "33"
+let score = "33abc"
+let score1 = null
 
 console.log(typeof score); // typeof is used to find the data type of the variable or any value that is given as its argument
 console.log(typeof(score)); // can be used as a function as well
 
 let valueasNum = Number(score) // conversion to NUMBER
+// conversion of a string which is not a pure number such as 33abc can lead to errors so check carefully
 
+let valueasNum1 = Number(score1)
+console.log(typeof valueasNum)
 console.log(valueasNum)
-console.log(score)
+console.log(valueasNum1)
+
 
