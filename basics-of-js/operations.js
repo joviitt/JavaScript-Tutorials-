a94f80ce89f9c987d9bb0640d70425ab56675043
joviitt <jovitjohn2004@gmail.com>
2025-12-27 17:59:38 +0530

    basic ops

--- basics-of-js/operations.js
+++ basics-of-js/operations.js
@@ -14,13 +14,30 @@ console.log(2/2)
 console.log(2%2)
 */
 let num = 2+1+"3"
-console.log(num);
-console.log(typeof num)
+// console.log(num);
+// console.log(typeof num)
 
-console.log(2+"2");
-console.log("1"+2);
-console.log("1"+2+2);
-console.log(1+2+"2");
+// console.log(2+"2");
+// console.log("1"+2);
+// console.log("1"+2+2); // if string appears first every object will be taken as string
+// console.log(1+2+"2"); // if a number appears first the numerical op is carried out first and then concatenated to the string
 
+
+// console.log(+true) // output is 1
+// console.log(+"") // output is 0
+
+// the two lines of code above are not good practices because its readability is low and its confusing
+
+let num1, num2 , num3
+
+// num1=num2=num3=2+2 // this is also bad practice as it is confusing for other devs
+
+num1 = 3
+console.log(num1++) // 
+console.log(num1)
+
+
+num2 = 3
+console.log(++num2)
 
 
