0f1aa338f10d5b4d15900279b6f9222e38813642
joviitt <jovitjohn2004@gmail.com>
2025-12-28 00:25:55 +0530

    nonprimitive datatypes

--- basics-of-js/datatypes-summary.js
+++ basics-of-js/datatypes-summary.js
@@ -11,7 +11,52 @@
 
         usually 7 Types: String, Number(decimal also comes under number), Boolean, Null(empty), Undefined, Symbol,
          ,BigInt(very big int values)
+        - 0 and null are different
     
-    2.
+    2.Reference DT(Non Primitive):
+    - they are usually call by reference meaning their original value is called from memory. it receives the address of the
+        variable instead of a copy. you can directly modify the original value
+
+        TYPES: Array, Object, Function
+        typeof() for these datatypes usually returns Object except for function(returns Function)
 
 */
+
+let userIn; // type of userIn is undefined
+
+// symbol declaration
+
+const id = Symbol('1234')
+const id2 = Symbol('1234')
+
+// even if both symbol have the same value(eg string or number), the value which is returned would be different. 
+// symbol is used to make an element unique.
+
+console.log(id === id2); // returns False
+
+const bigNumber = 34n
+// n is used to denote bigint
+console.log(typeof bigNumber);
+
+let myObj = {
+    name: "jovit",
+    age: 21
+} // object
+
+let movies = ["21 jump street", "22 jump street"] // array
+
+// console.log(typeof movies);  returns object
+
+
+let myFunction = Function()
+{
+    console.log("Hello World")
+} // function
+
+myFunction()
+
+
+
+
+
+
