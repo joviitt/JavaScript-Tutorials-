ffb3f1156e8fbc6dab4bfa12ac624e9f702434dd
joviitt <jovitjohn2004@gmail.com>
2025-12-28 00:01:38 +0530

    primitive datatypes

---
+++ basics-of-js/datatypes-summary.js
@@ -0,0 +1,17 @@
+/*
+
+    Data types, on the basis of how they are stored and accessed, can be categorized into 2 parts(officially):
+
+    1. Primitive
+    2. Non-Primitive
+
+    1.Primitive DT:
+    - they are usuallly call by value(meaning a copy of the actual value is returned and any change to the copy does not
+        affect the original value)
+
+        usually 7 Types: String, Number(decimal also comes under number), Boolean, Null(empty), Undefined, Symbol,
+         ,BigInt(very big int values)
+    
+    2.
+
+*/
