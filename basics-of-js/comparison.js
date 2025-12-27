a7d03fb508f06988bb92cd2aac9fadabd40c5039
joviitt <jovitjohn2004@gmail.com>
2025-12-27 23:52:20 +0530

    strict checking

--- basics-of-js/comparison.js
+++ basics-of-js/comparison.js
@@ -9,3 +9,28 @@ console.log(2>=1)
 console.log(2>1)
 */
 
+// console.log("2"<2); //false
+// console.log("02"<3); // true
+
+//typescript doesnt allow comparisons between diff data types. js allows that
+
+// console.log(null > 0); // false
+// console.log(null >= 0); // true
+// console.log(null == 0); // false
+
+// the equality check(==) and comparison op(<,>,<=,>=) work differently. sometimes null is converted to NaN ans sometimes
+// it is converted to 0 that is why the middle statement above returns true
+
+// console.log(undefined == null); //true
+// console.log(undefined > 0); //false
+// console.log(undefined == 0); //false
+// console.log(undefined <= 0); //false
+
+console.log("2"=== 2); // === is the strict check op. it also checks for the datatype of the operands
+console.log("2"=== "2");
+console.log("2"=== "3"); 
+
+
+
+
+
