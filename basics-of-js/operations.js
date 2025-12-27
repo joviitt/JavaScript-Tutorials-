e944df6a075ae62f36b1fd1f0ec2b231ba7c455f
joviitt <jovitjohn2004@gmail.com>
2025-12-27 11:43:12 +0530

    concatenation

--- basics-of-js/operations.js
+++ basics-of-js/operations.js
@@ -1,5 +1,26 @@
 let value = 33
 let negValue = -value  // by using the hyphen(-) we can negate any integer value
-console.log(negValue);
-console.log(typeof negValue);
+// console.log(negValue);
+// console.log(typeof negValue);
+
+/*
+BASIC OPERATIONS:
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+*/
+let num = 2+1+"3"
+console.log(num);
+console.log(typeof num)
+
+console.log(2+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
 
