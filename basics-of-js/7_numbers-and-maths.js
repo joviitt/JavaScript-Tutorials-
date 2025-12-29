060febd57c05501933fedca40a76f01c56ae5257
joviitt <jovitjohn2004@gmail.com>
2025-12-29 23:26:16 +0530

    numbers

---
+++ basics-of-js/7_numbers-and-maths.js
@@ -0,0 +1,19 @@
+const score =400
+
+const newScore = new Number(100) // this is a way of explicitly declaring a number
+
+console.log(score);
+console.log(newScore);
+
+console.log(newScore.toString());//converting number to string
+console.log(typeof newScore);
+
+console.log(score.toFixed(2)); //digits after the decimal point, returns a string
+
+const otherNumber = 23.6186385
+
+console.log(otherNumber.toPrecision(3)) // returns the closest whole number whose length is 2
+
+console.log(Number.MAX_VALUE) // returns max possible value of number
+
+
